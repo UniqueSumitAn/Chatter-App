2a6e1930c86024025ca59e5961b7b475a4c2ff26
SUMIT <sumitkrofficial332@gmail.com>
2025-11-18 22:01:01 +0530

    test api included

--- backend/server.js
+++ backend/server.js
@@ -5,39 +5,42 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectdb = require("./lib/db");
 const userRouter = require("./routes/userRoutes");
-const {chat} = require("./controllers/messageController");
+const { chat } = require("./controllers/messageController");
 // const websocket = require("./webSocket/websocket");
-
 
 const app = express();
 const server = http.createServer(app);
 chat(server);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 const allowedOrigins = [
-  "http://localhost:5173",          // Local development
-  "https://chatter-app-mu.vercel.app/" // Vercel frontend
+  "http://localhost:5173", // Local development
+  "https://chatter-app-mu.vercel.app/", // Vercel frontend
 ];
 
 app.use(
   cors({
     origin: allowedOrigins, // simple array whitelist
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,      // allow cookies and auth headers
+    credentials: true, // allow cookies and auth headers
   })
 );
 
-
 //routes
 app.use("/user", userRouter);
+app.get("/testapi", async (req, res) => {
+  console.log("hii");
+  return res.json({ success: true, message: "server is live" });
+});
 
 // connect mongoose database
 connectdb();
 
 // WebSocket(server);
- if(process.env.NODE_ENV!=="production")
-{const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("server connected: ", PORT);
-});}
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log("server connected: ", PORT);
+  });
+}
 export default server;
