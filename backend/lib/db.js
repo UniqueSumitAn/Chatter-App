51b1039dddfd38d09b4047d089f73108df5b883b
SUMIT <sumitkrofficial332@gmail.com>
2025-11-19 18:26:25 +0530

    database connnecting setting

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectdb = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/CHATTER`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     ("database connected");
 
     console.log("database connected");
